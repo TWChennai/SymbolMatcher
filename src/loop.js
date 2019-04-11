6aac6e6813ff8d6d39115f11346248591fc1ffd0
aravindpl <aravind.pl.kumar@gmail.com>
2019-04-11 15:05:29 +0530

    Highlight pointed symbol.

--- src/loop.js
+++ src/loop.js
@@ -1,33 +1,37 @@
 import * as frameUtils from './frameUtils.js'
 import * as utils from './utils.js'
-import * as symbolUtils from './symbols_init.js'
+import * as symbolHelper from './symbolHelper.js'
 
 var controllerOptions = {
   enableGestures: true
 };
 
 $(function () {
-  symbolUtils.initSymbol()
+  symbolHelper.initSymbol()
+
   Leap.loop(controllerOptions, function (frame) {
-
-    var frameString = "Frame ID: " + frame.id + "<br />" +
-      "Timestamp: " + frame.timestamp + " &micro;s<br />";
-    $('#frameData').html(frameString)
-
     setBasicValues(frame);
-    if (frame.pointables.length > 0) {
-      const windowCoordinates = frameUtils.getRightIndexFingerCoordinates(frame);
-      setWindowCoordinates(windowCoordinates);
-      if (!Number.isNaN(windowCoordinates[0] && !Number.isNaN(windowCoordinates[1]))){
-        var eleInPosition = document.elementFromPoint(windowCoordinates[0], windowCoordinates[1])
-        if (eleInPosition != null )
-        $('#currentEle').html(eleInPosition.className)
-      }
-    }
+    showPointedObject(frame);
   })
 });
 
 
+
+function showPointedObject(frame) {
+  if (frame.pointables.length > 0) {
+    const windowCoordinates = frameUtils.getRightIndexFingerCoordinates(frame);
+    setWindowCoordinates(windowCoordinates);
+    if (!Number.isNaN(windowCoordinates[0] && !Number.isNaN(windowCoordinates[1]))) {
+      var eleInPosition = document.elementFromPoint(windowCoordinates[0], windowCoordinates[1]);
+      if (eleInPosition != null)
+        // $('#currentEle').html(eleInPosition.className);
+        if ($(eleInPosition).hasClass("symbol")) {
+          $('.symbol').removeClass("selected")
+          $(eleInPosition).addClass("selected")
+        }
+    }
+  }
+}
 
 function setWindowCoordinates(windowPosition) {
   var windowCoordinates = document.getElementById('windowCoordinates');
