41cfb69e8ba6fb99c8f2e7a8e91a25a3bd133c8b
aravindpl <aravind.pl.kumar@gmail.com>
2019-04-17 13:09:08 +0530

    Fix styling for symbol and company

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -6,58 +6,59 @@ var symbols = [{
     },
     {
         name: 'Symbol2',
-        symbolImageUrl: 'url2',
-        companyImageUrl: 'url2',
+        symbolImageUrl: 'img/selenium_thoughtworks.jpeg',
+        companyImageUrl: 'img/selenium_thoughtworks.jpeg',
         companyName: 'Company2'
     },
     {
         name: 'Symbol3',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
+        symbolImageUrl: 'img/selenium_thoughtworks.jpeg',
+        companyImageUrl: 'img/selenium_thoughtworks.jpeg',
         companyName: 'Company3'
     },
     {
         name: 'Symbol4',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
+        symbolImageUrl: 'img/selenium_thoughtworks.jpeg',
+        companyImageUrl: 'img/selenium_thoughtworks.jpeg',
         companyName: 'Company4'
     },
     {
         name: 'Symbol5',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
+        symbolImageUrl: 'img/selenium_thoughtworks.jpeg',
+        companyImageUrl: 'img/selenium_thoughtworks.jpeg',
         companyName: 'Company5'
-    },
-    {
-        name: 'Symbol6',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
-        companyName: 'Company6'
-    },
-    {
-        name: 'Symbol7',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
-        companyName: 'Company7'
-    },
-    {
-        name: 'Symbol8',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
-        companyName: 'Company8'
-    },
-    {
-        name: 'Symbol9',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
-        companyName: 'Company9'
-    },
-    {
-        name: 'Symbol10',
-        symbolImageUrl: 'url3',
-        companyImageUrl: 'url3',
-        companyName: 'Company10'
     }
+    // },
+    // {
+    //     name: 'Symbol6',
+    //     symbolImageUrl: 'url3',
+    //     companyImageUrl: 'url3',
+    //     companyName: 'Company6'
+    // },
+    // {
+    //     name: 'Symbol7',
+    //     symbolImageUrl: 'url3',
+    //     companyImageUrl: 'url3',
+    //     companyName: 'Company7'
+    // },
+    // {
+    //     name: 'Symbol8',
+    //     symbolImageUrl: 'url3',
+    //     companyImageUrl: 'url3',
+    //     companyName: 'Company8'
+    // },
+    // {
+    //     name: 'Symbol9',
+    //     symbolImageUrl: 'url3',
+    //     companyImageUrl: 'url3',
+    //     companyName: 'Company9'
+    // },
+    // {
+    //     name: 'Symbol10',
+    //     symbolImageUrl: 'url3',
+    //     companyImageUrl: 'url3',
+    //     companyName: 'Company10'
+    // }
 ]
 
 var getSymbols = function () {
