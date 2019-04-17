377de3399f19a6a255221bc4ca7ac5b4e3b3dad5
aravindpl <aravind.pl.kumar@gmail.com>
2019-04-17 14:54:42 +0530

    Style the game stats

--- src/game.js
+++ src/game.js
@@ -20,7 +20,7 @@ var progress = function () {
     view.handleEachRound()
     var isgameOver = state.isGameOver()
     if (isgameOver) {
-        // alert("Status: " + state.shortStats())
+        alert("Status: " + state.shortStats())
         initGame()
         return;
     }
--- src/scoreboardHelper.js
+++ src/scoreboardHelper.js
@@ -6,8 +6,12 @@ var init = function() {
 
 var update = function() {
     var stats = state.stats()
-    var summary = "Status: " + stats.status + " Total Symbols: " + stats.totalSymbols + " Symbols Matched: " + stats.successCount + " Trials left: " + stats.trialsLeft
-    $('#summary').text(summary)
+    var statusEle = $("<p>").html("<b>Status:</b> " + stats.status)
+    var totalEle = $("<p>").html("<b>Total Symbols:</b> " + stats.totalSymbols)
+    var successEle = $("<p>").html("<b>Symbols Matched:</b> " + stats.successCount)
+    var trialsEle = $("<p>").html("<b>Trials left:</b> " + stats.trialsLeft)
+    $('#summary').html('')
+    $('#summary').append(statusEle).append(totalEle).append(successEle).append(trialsEle)
 }
 
 export { init, update }
